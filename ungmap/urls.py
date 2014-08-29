68ddaf050fb4de2abb0074d945d00a04c239d867
wc351 <wc351@yahoo.com>
2014-08-28 20:33:01 -0400

    Created the first html page.
    Created the view for the main page.
    Added the url for the main page.
    created a template for the main page.

--- ungmap/urls.py
+++ ungmap/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from apps.views import MainView
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ungmap.views.home', name='home'),
+    url(r'^$', MainView.as_view(), name='main_page'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
